d3f0c454517d1cda7ef044a3a69ef42beab8815a
Samarth Pansala <131592070+pansalasamarth@users.noreply.github.com>
2024-05-13 11:44:00 +0530

    Update db.js

--- Utils/db.js
+++ Utils/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const DB = "mongodb+srv://samarthpansala:Samarth_0645@cluster0.n8jvrme.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // const DB = "mongodb://localhost:27017/DogWorld";
-
+ 
 
 const connectDb = async() => {
 
